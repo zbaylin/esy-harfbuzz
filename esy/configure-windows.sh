07443630146ddfd28f7f372701fc368aaae9572a
Bryan Phelps <bryphe@outlook.com>
2020-08-12 13:17:47 -0700

    Try getting out of the cmake business

--- esy/configure-windows.sh
+++ esy/configure-windows.sh
@@ -2,6 +2,4 @@ cd _build
 
 INSTALL_PATH="$(cygpath -u $cur__install)"
 
-echo "Install: $INSTALL_PATH"
-
-cmake -G "Unix Makefiles" ../harfbuzz-2.6.8 -DCMAKE_C_COMPILER=x86_64-w64-mingw32-gcc -DCMAKE_CXX_COMPILER=x86_64-w64-mingw32-g++ -DCMAKE_LINKER=/usr/bin/x86_64-w64-mingw32-ld -DCMAKE_AR=/usr/bin/x86_64-w64-mingw32-ar -DCMAKE_INSTALL_PREFIX=$INSTALL_PATH
+./configure --prefix=$cur__install --host=x86_64-w64-mingw32
